425f54e6209350ed70ce5b7c57c934026bc00120
aidanross430 <aidanross430@gmail.com>
2026-02-16 00:23:31 -0500

    Added sample of parsing client-side json file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import FetchCoffeeImage from "./CoffeeAPI.jsx";
 import LeafletMap from "./LeafletMap.jsx";
 import PullCSV from "./CQIData.jsx";
+import PrintJson from "./ParseJson.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -16,5 +17,7 @@ createRoot(document.getElementById('root')).render(
         <LeafletMap />
         <h2>CQI Database has been printed to console</h2>
         <PullCSV />
+        <h2>Json data held client-side has been printed to console</h2>
+        <PrintJson />
     </StrictMode>,
 )
